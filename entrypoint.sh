9db798fc0baaa6bbf1592e765e66d7cc3b30b436
lovehifi <81028393+lovehifi@users.noreply.github.com>
2023-10-05 13:40:17 +0700

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,17 +1,25 @@
 #!/bin/bash
 
 #export LD_LIBRARY_PATH=/app/ifi-tidal-release/lib
-#echo "Starting Speaker Application in Background (TMUX)"
-#/usr/bin/tmux new-session -d -s speaker_controller_application '/app/ifi-tidal-release/bin/speaker_controller_application'
+echo "Starting Speaker Application in Background (TMUX)"
+/usr/bin/tmux new-session -d -s speaker_controller_application '/app/ifi-tidal-release/bin/speaker_controller_application'
+
+# Extract the card number from rAudio
+card=$(grep -oP '(?<=defaults.pcm.card ).*' /etc/asound.conf)
+echo "Card number: $card"
+
+# Extract the corresponding device info for the card number
+device_info=$(awk -v card_num="$card" -F= '$1 ~ "device#"card_num {print $2}' <( /app/ifi-tidal-release/bin/ifi-pa-devs-get ))
+echo "Device info for the card number: $device_info"
 
 echo "Starting TIDAL Connect.."
 /app/ifi-tidal-release/bin/tidal_connect_application \
    --tc-certificate-path "/app/ifi-tidal-release/id_certificate/IfiAudio_ZenStream.dat" \
-   --playback-device "default" \
-   -f "rAudio" \
+   --playback-device "$device_info" \
+   -f "rAudioPi4" \
    --codec-mpegh true \
    --codec-mqa false \
-   --model-name "Pi" \
+   --model-name "Pi4" \
    --disable-app-security false \
    --disable-web-security false \
    --enable-mqa-passthrough false \
